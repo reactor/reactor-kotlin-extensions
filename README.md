aca181d8aa12487190af2aa25bfc898053902ece
Simon Baslé <sbasle@pivotal.io>
2019-05-07 17:21:18 +0200

    Prepare 1.0.0.M1

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# reactor-kotlin-extensions
-Reactor Kotlin Support
+# Reactor Kotlin Extensions
+
+[![Join the chat at https://gitter.im/reactor/reactor](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/reactor/reactor?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+Add various extensions and adapters for Kotlin programming language. While Reactor Core, Reactor Addons and other Reactor libraries 
+are designed to be Kotlin friendly with the use of `@Nullable` and carefully picked signature types, Kotlin developers might 
+want to adapt the core functional programming API further.
+
+# Maven Artifacts
+
+With Gradle from repo.spring.io or Maven Central repositories (stable releases only):
+
+```groovy
+    repositories {
+//      maven { url 'https://repo.spring.io/snapshot' }
+      maven { url 'https://repo.spring.io/milestone' }
+      mavenCentral()
+    }
+
+    dependencies {
+      //compile "io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.0.BUILD-SNAPSHOT"
+      compile "io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.0.M1"
+    }
+```
+
+# Examples
+
+Tuple destructure:
+```kotlin
+val (t1, t2, t3) = Tuples.of(O1, O2, O3)
+assertEquals(t1, O1)
+assertEquals(t2, O2)
+assertEquals(t3, O3)
+```
+
+Convert, sum (*requires reactor-extra*) and test (*requires reactor-test*):
+```kotlin
+ intArrayOf(2_000_000_000, 200_000_000) //sum overflows an Int
+                .toFlux()
+                .sum()
+                .test()
+                .expectNext(2_200_000_000)
+                .verifyComplete()
+```
+
+Coordinating when two mono completes:
+```kotlin
+whenComplete("foo1".toMono(), "foo2".toMono())
+	.test()
+	.verifyComplete()
+```
+
+## License
+
+Reactor is [Apache 2.0 licensed](https://www.apache.org/licenses/LICENSE-2.0.html).
--- src/test/kotlin/reactor/kotlin/adapter/rxjava/RxJava2AdapterExtTest.kt
+++ src/test/kotlin/reactor/kotlin/adapter/rxjava/RxJava2AdapterExtTest.kt
@@ -30,136 +30,136 @@ class RxJava2AdapterExtTest {
     @Test
     fun `Flowable to Flux`() {
         Flowable.range(1, 10)
-            .toFlux()
-            .test()
-            .expectFusion()
-            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            .expectComplete()
-            .verify()
+                .toFlux()
+                .test()
+                .expectFusion()
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Flux to Flowable`() {
         Flux.range(1, 10)
-            .toFlowable()
-            .test()
-            .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            .assertComplete()
+                .toFlowable()
+                .test()
+                .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .assertComplete()
     }
 
     @Test
     fun `Mono to Flowable`() {
         Mono.just(1)
-            .toFlowable()
-            .test()
-            .assertValues(1)
-            .assertNoErrors()
-            .assertComplete()
+                .toFlowable()
+                .test()
+                .assertValues(1)
+                .assertNoErrors()
+                .assertComplete()
     }
 
     @Test
     fun `Mono to Completable`() {
         Mono.empty<Any>()
-            .toCompletable()
-            .test()
-            .assertNoValues()
-            .assertNoErrors()
-            .assertComplete()
+                .toCompletable()
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertComplete()
     }
 
     @Test
     fun `Completable to Mono`() {
         Completable.complete()
-            .toMono()
-            .test()
-            .expectComplete()
-            .verify()
+                .toMono()
+                .test()
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Mono to Single`() {
         Mono.just(1)
-            .toSingle()
-            .test()
-            .assertValues(1)
-            .assertNoErrors()
-            .assertComplete()
+                .toSingle()
+                .test()
+                .assertValues(1)
+                .assertNoErrors()
+                .assertComplete()
     }
 
     @Test
     fun `Empty Mono to Single`() {
         Mono.empty<Int>()
-            .toSingle()
-            .test()
-            .assertNoValues()
-            .assertError(NoSuchElementException::class.java)
-            .assertNotComplete()
+                .toSingle()
+                .test()
+                .assertNoValues()
+                .assertError(NoSuchElementException::class.java)
+                .assertNotComplete()
     }
 
     @Test
     fun `Single to Mono`() {
         Single.just(1)
-            .toMono()
-            .test()
-            .expectFusion(Fuseable.ANY, Fuseable.ASYNC)
-            .expectNext(1)
-            .expectComplete()
-            .verify()
+                .toMono()
+                .test()
+                .expectFusion(Fuseable.ANY, Fuseable.ASYNC)
+                .expectNext(1)
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Observable to Flux`() {
         Observable.range(1, 10)
-            .toFlux()
-            .test()
-            .expectFusion()
-            .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            .expectComplete()
-            .verify()
+                .toFlux()
+                .test()
+                .expectFusion()
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Flux to Observable`() {
         Flux.range(1, 10)
-            .toObservable()
-            .test()
-            .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            .assertNoErrors()
-            .assertComplete()
+                .toObservable()
+                .test()
+                .assertValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .assertNoErrors()
+                .assertComplete()
     }
 
     @Test
     fun `Maybe to Mono`() {
         Maybe.just(1)
-            .toMono()
-            .test()
-            .expectNext(1)
-            .expectComplete()
-            .verify()
+                .toMono()
+                .test()
+                .expectNext(1)
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Empty Maybe to Mono`() {
         Maybe.empty<Void>()
-            .toMono()
-            .test()
-            .expectComplete()
-            .verify()
+                .toMono()
+                .test()
+                .expectComplete()
+                .verify()
     }
 
     @Test
     fun `Mono to Maybe`() {
         Mono.just(1)
-            .toMaybe()
-            .test()
-            .assertResult(1)
+                .toMaybe()
+                .test()
+                .assertResult(1)
     }
 
     @Test
     fun `Empty Mono to Maybe`() {
         Mono.empty<Any>()
-            .toMaybe()
-            .test()
-            .assertResult()
+                .toMaybe()
+                .test()
+                .assertResult()
     }
 }
