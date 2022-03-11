c744c55bad75c1135486f545c54164ed9c32f52e
Simon Baslé <sbasle@vmware.com>
2022-03-11 10:51:53 +0100

    polish deprecation message

--- src/main/kotlin/reactor/kotlin/extra/swing/SwtSchedulerExtensions.kt
+++ src/main/kotlin/reactor/kotlin/extra/swing/SwtSchedulerExtensions.kt
@@ -25,8 +25,8 @@ import reactor.swing.SwtScheduler
  *
  * @author Simon Baslé
  * @since 3.1.1
- * @deprecated To be removed in 3.5.0 (fast-tracked). See https://github.com/reactor/reactor-addons/issues/273
+ * @deprecated To be removed aggressively in 3.5.0. See https://github.com/reactor/reactor-addons/issues/273
  */
-@Deprecated(message = "To be removed exceptionally fast in 3.5.0. SWT support is being removed in addons",
+@Deprecated(message = "To be removed aggressively in 3.5.0. SWT support is being removed in addons",
     level = DeprecationLevel.ERROR)
 fun Display.toScheduler(): Scheduler = SwtScheduler.from(this)
